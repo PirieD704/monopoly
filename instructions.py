05c360c07cc3f0eea8aae248811ec82de139bddf
PirieD704 <dp75975@gmail.com>
2016-10-11 19:23:22 -0400

    added instructions page

---
+++ instructions.py
@@ -0,0 +1,28 @@
+# Algorithm while I catch up
+# Write a Python script to simulate a monopoly game between two players
+# Each player should take 1000 turns (not rolls) 
+# No bank is kept
+# A turn consists of a roll of two 6 sided dice
+# if doubles are rolled, the player goes again
+# Each roll should be captured to mysql
+# Include any extra columns but you must include
+# Which player
+# Roll total
+# Space they landed on as an integer or text
+# Bonus
+# if the player lands are square 30 the player goes to jail
+# This will require a "space ended on” column
+# Include Chance and Community chest cards
+
+# When the game is complete, I want a print out of:
+# the most common roll per player
+# The most common property landed on 
+# How often a player lands on square 30 (“Go to Jail”) vs. Free Parking
+# How often does the player land on Boardwalk vs. St. Charles Place? St. Charles Place cost $140, Boardwalk costs $400. Which is “better” economically?
+# How much money the player makes passing GO (at $200 per pass)
+# What is the difference in spaces moved between player 1 and player 2
+# BONUS: The property that generates the most rental income after the game passes 100 turns, assuming all properties have hotels. See https://en.wikibooks.org/wiki/Monopoly/Official_Rules
+# BONUS: The distribution of each roll (% of 2, % of 3, etc.)
+# SUPER BONUS: The distribution of each monopoly landed on (% of light blue, purple, orange, etc.)
+# Import flask stuff
+
